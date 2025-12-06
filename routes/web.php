c1810345a3d57443c728318718f381395ad5929e
Rio Adrian <rioadriansky02@gmail.com>
2025-12-06 14:22:39 +0700

    Belajar redirect ke route ataupun ke controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/logout', function () {
+    return redirect()->action([HomeController::class, 'index'], ['sourceUrl' => 'Logout']);
+});
+
+
+
+
